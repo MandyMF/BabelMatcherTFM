c17aa1fe4d630d6df46d173bc36a69828826c000
MandyMF <mandy@fonoma.com>
2023-08-15 23:26:27 +0200

    Added to function the execution of test .py and not __main__ is used

--- process_by_all_lemmas_with_padchest_data.py
+++ process_by_all_lemmas_with_padchest_data.py
@@ -1,57 +1,61 @@
 from BabelMatcher import BabelTermsMatcher
 from utils import get_data_from_config_file, write_result_to_pc, create_html_file, create_html_var, write_html_to_pc, write_html
 
-config_data = get_data_from_config_file()
+def exec():
+  config_data = get_data_from_config_file()
 
-testBabelTermsMatcher = BabelTermsMatcher(
-                                          config_data['babel_key'], 
-                                          waitTime= config_data['waiting_time_on_error'], 
-                                          doNotWaitForServer= config_data['not_wait_when_token_are_spend'], 
-                                          matchDistance=config_data['match_levenshtein_distance'],
-                                          is_lemmatized= config_data['dataset_is_lemmatized']
-                                        )
-testBabelTermsMatcher.load_data_from_padchest(config_data['data_to_process_path'])
+  testBabelTermsMatcher = BabelTermsMatcher(
+                                            config_data['babel_key'], 
+                                            waitTime= config_data['waiting_time_on_error'], 
+                                            doNotWaitForServer= config_data['not_wait_when_token_are_spend'], 
+                                            matchDistance=config_data['match_levenshtein_distance'],
+                                            is_lemmatized= config_data['dataset_is_lemmatized']
+                                          )
+  testBabelTermsMatcher.load_data_from_padchest(config_data['data_to_process_path'])
 
-data_from_babelnet = testBabelTermsMatcher.get_data_from_list_of_lemmas_default_all ( config_data['lemma_list'], config_data['lang'], config_data['search_levels'])
+  data_from_babelnet = testBabelTermsMatcher.get_data_from_list_of_lemmas_default_all ( config_data['lemma_list'], config_data['lang'], config_data['search_levels'])
 
-write_result_to_pc(data_from_babelnet, config_data['save_pattern_path'])
+  write_result_to_pc(data_from_babelnet, config_data['save_pattern_path'])
 
-model, ruler = testBabelTermsMatcher.create_NER_model(data_from_babelnet, config_data['lang'])
+  model, ruler = testBabelTermsMatcher.create_NER_model(data_from_babelnet, config_data['lang'])
 
-testBabelTermsMatcher.save_current_model(config_data['save_model_path'])
+  testBabelTermsMatcher.save_current_model(config_data['save_model_path'])
 
-results = []
-raw_results = []
+  results = []
+  raw_results = []
 
-print("Model Created and Saved")
-print("Now Processing Data")
+  print("Model Created and Saved")
+  print("Now Processing Data")
 
-for i in range(len(testBabelTermsMatcher.data)):
-  if(type(testBabelTermsMatcher.data[i]) != str):
-    continue
+  for i in range(len(testBabelTermsMatcher.data)):
+    if(type(testBabelTermsMatcher.data[i]) != str):
+      continue
 
-  doc = testBabelTermsMatcher.apply_model(model, testBabelTermsMatcher.data[i])
-  results.append({ "id": testBabelTermsMatcher.ImageIds[i] ,"tags":[{"entity": str(ent.text) , "tag": str(ent.label_)} for ent in doc.ents]})
-  raw_results.append(doc)
+    doc = testBabelTermsMatcher.apply_model(model, testBabelTermsMatcher.data[i])
+    results.append({ "id": testBabelTermsMatcher.ImageIds[i] ,"tags":[{"entity": str(ent.text) , "tag": str(ent.label_)} for ent in doc.ents]})
+    raw_results.append(doc)
 
-print("Saving Results")
-write_result_to_pc(results, config_data['result_file_path'])
+  print("Saving Results")
+  write_result_to_pc(results, config_data['result_file_path'])
 
-create_html_file(config_data['save_html_view'])
+  create_html_file(config_data['save_html_view'])
 
-batch_size= 100
-count = 0
-html_result = ''
+  batch_size= 100
+  count = 0
+  html_result = ''
 
-print("Creating Html Data from Results")
+  print("Creating Html Data from Results")
 
-for doc in raw_results:
-  _, html = testBabelTermsMatcher.display_html_doc_by_labels(doc)
-  html_result = create_html_var(html, html_result, results[count]["id"])
-  count += 1
-  if(count % batch_size == 0):
-    write_html_to_pc(html_result, config_data['save_html_view'])
+  for doc in raw_results:
+    _, html = testBabelTermsMatcher.display_html_doc_by_labels(doc)
+    html_result = create_html_var(html, html_result, results[count]["id"])
+    count += 1
+    if(count % batch_size == 0):
+      write_html_to_pc(html_result, config_data['save_html_view'])
 
-write_html_to_pc(html_result, config_data['save_html_view'])
+  write_html_to_pc(html_result, config_data['save_html_view'])
 
-print("Job Done")
+  print("Job Done")
+
+if __name__ == "__main__":
+  exec ()
--- process_with_saved_model.py
+++ process_with_saved_model.py
@@ -1,53 +1,57 @@
 from BabelMatcher import BabelTermsMatcher
 from utils import get_data_from_config_file, write_result_to_pc, create_html_file, create_html_var, write_html_to_pc, write_html
 
-config_data = get_data_from_config_file()
+def exec():
+  config_data = get_data_from_config_file()
 
-testBabelTermsMatcher = BabelTermsMatcher(
-                                          config_data['babel_key'], 
-                                          waitTime= config_data['waiting_time_on_error'], 
-                                          doNotWaitForServer= config_data['not_wait_when_token_are_spend'], 
-                                          matchDistance=config_data['match_levenshtein_distance'],
-                                          is_lemmatized= config_data['dataset_is_lemmatized']
-                                        )
-testBabelTermsMatcher.load_data_from_padchest(config_data['data_to_process_path'])
+  testBabelTermsMatcher = BabelTermsMatcher(
+                                            config_data['babel_key'], 
+                                            waitTime= config_data['waiting_time_on_error'], 
+                                            doNotWaitForServer= config_data['not_wait_when_token_are_spend'], 
+                                            matchDistance=config_data['match_levenshtein_distance'],
+                                            is_lemmatized= config_data['dataset_is_lemmatized']
+                                          )
+  testBabelTermsMatcher.load_data_from_padchest(config_data['data_to_process_path'])
 
-testBabelTermsMatcher.load_to_current_model(config_data['load_model_path'])
+  testBabelTermsMatcher.load_to_current_model(config_data['load_model_path'])
 
-testBabelTermsMatcher.save_current_model(config_data['save_model_path'])
+  testBabelTermsMatcher.save_current_model(config_data['save_model_path'])
 
-results = []
-raw_results = []
+  results = []
+  raw_results = []
 
-print("Model Loaded and Saved")
-print("Now Processing Data")
+  print("Model Loaded and Saved")
+  print("Now Processing Data")
 
-for i in range(len(testBabelTermsMatcher.data)):
-  if(type(testBabelTermsMatcher.data[i]) != str):
-    continue
-  
-  doc = testBabelTermsMatcher.apply_model(testBabelTermsMatcher.currentNER_Model, testBabelTermsMatcher.data[i])
-  results.append({ "id": testBabelTermsMatcher.ImageIds[i] ,"tags":[{"entity": str(ent.text) , "tag": str(ent.label_)} for ent in doc.ents]})
-  raw_results.append(doc)
+  for i in range(len(testBabelTermsMatcher.data)):
+    if(type(testBabelTermsMatcher.data[i]) != str):
+      continue
+    
+    doc = testBabelTermsMatcher.apply_model(testBabelTermsMatcher.currentNER_Model, testBabelTermsMatcher.data[i])
+    results.append({ "id": testBabelTermsMatcher.ImageIds[i] ,"tags":[{"entity": str(ent.text) , "tag": str(ent.label_)} for ent in doc.ents]})
+    raw_results.append(doc)
 
-print("Saving Results")
-write_result_to_pc(results, config_data['result_file_path'])
+  print("Saving Results")
+  write_result_to_pc(results, config_data['result_file_path'])
 
-create_html_file(config_data['save_html_view'])
+  create_html_file(config_data['save_html_view'])
 
-batch_size = 100
-count = 0
-html_result = ''
+  batch_size = 100
+  count = 0
+  html_result = ''
 
-print("Creating Html Data from Results")
+  print("Creating Html Data from Results")
 
-for doc in raw_results:
-  _, html = testBabelTermsMatcher.display_html_doc_by_labels(doc)
-  html_result = create_html_var(html, html_result, results[count]["id"])
-  count += 1
-  if(count % batch_size == 0):
-    write_html_to_pc(html_result, config_data['save_html_view'])
+  for doc in raw_results:
+    _, html = testBabelTermsMatcher.display_html_doc_by_labels(doc)
+    html_result = create_html_var(html, html_result, results[count]["id"])
+    count += 1
+    if(count % batch_size == 0):
+      write_html_to_pc(html_result, config_data['save_html_view'])
 
-write_html_to_pc(html_result, config_data['save_html_view'])
+  write_html_to_pc(html_result, config_data['save_html_view'])
 
-print("Job Done")
+  print("Job Done")
+
+if __name__ == "__main__":
+  exec ()
